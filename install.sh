798ab56f4612f4412266fb28a722604afa23ced0
Jordan Pyott <jordanpyott@gmail.com>
2022-09-18 17:59:42 +1200

    RELEASE: bump version to 0.3 -- using nvchad config

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker pull atidyshirt/jordanp-env:0.2
-docker run -it -v $(pwd):/home/ws --detach-keys="ctrl-d" -v ~/.gitconfig:/etc/gitconfig atidyshirt/jordanp-env:0.2 /bin/zsh
+docker pull atidyshirt/jordanp-env:0.3
+docker run -it -v $(pwd):/home/ws --detach-keys="ctrl-d" -v ~/.gitconfig:/etc/gitconfig atidyshirt/jordanp-env:0.3 /bin/zsh
