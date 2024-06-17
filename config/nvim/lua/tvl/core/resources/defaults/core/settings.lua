41462f21dce4facc0492b3dbe5f99097634ad7d9
jordanp <jordanpyott@gmail.com>
2024-06-17 23:04:24 +1200

    GH-1: Enable no icons for neovim config
    This patch Closes #1 - enables support for config without icons

--- config/nvim/lua/tvl/core/resources/defaults/core/settings.lua
+++ config/nvim/lua/tvl/core/resources/defaults/core/settings.lua
@@ -1,6 +1,6 @@
 local util = require("tvl.util")
 
-vim.g.nerd_font_enabled = true
+vim.g.nerd_font_enabled = false
 
 -- autocmds and keymaps can wait to load
 vim.api.nvim_create_autocmd("User", {
