f0576d4e5678208f92ac812099526018b427eb01
jordanp <jordanpyott@gmail.com>
2024-06-19 10:06:36 +0000

    ssh: mount .ssh directory to the container
    Add a mount to the workspace to inherit the ssh directory for git to use
    when pushing commits upstream

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it -v ~/.gitconfig:/etc/gitconfig -v $(pwd):/home/workspace --detach-keys="ctrl-x" atidyshirt/jordanp-env:latest
+docker run -it -v ~/.gitconfig:/etc/gitconfig -v ~/.ssh:/root/.ssh -v $(pwd):/home/workspace --detach-keys="ctrl-x" atidyshirt/jordanp-env:latest
