096f4992eedcc2445f8c45400b3dafe31ffcab67
jordanp <jordanpyott@gmail.com>
2024-06-20 14:26:51 +1200

    install: updating install script to allow for a container name
    Make sure we give the container a name and re-use the same container
    when we are working in the same directory. This will prevent us from
    create a new container each time we attach to a session.

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-docker run -it \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v ~/.gitconfig:/etc/gitconfig \
-  -v ~/.ssh:/root/.ssh \
-  -v $(pwd):/home/workspace \
-  --detach-keys="ctrl-x" \
-  atidyshirt/jordanp-env:latest
+CONTAINER_NAME="env-$(basename $(pwd) | sed 's/[^a-zA-Z0-9]/-/g')"
+CONTAINER_ID=$(docker ps -aqf name=$CONTAINER_NAME)
+
+if [[ -z "$CONTAINER_ID" ]]; then
+    docker run -it \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v ~/.gitconfig:/etc/gitconfig \
+      -v ~/.ssh:/root/.ssh \
+      -v $(pwd):/home/workspace \
+      --detach-keys="ctrl-x" \
+      --name $CONTAINER_NAME \
+      atidyshirt/jordanp-env:latest
+else
+    docker start -ai $CONTAINER_NAME
+fi
